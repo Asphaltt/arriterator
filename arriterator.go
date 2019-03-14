c318be45c7489b8662863301431729e9bb7ddfdb
Leon Huayra <hf@sangfor.com.cn>
2019-03-14 14:36:45 +0800

    use reflect.Value to store the array instead of unsafe.Pointer

--- arriterator.go
+++ arriterator.go
@@ -1,24 +1,23 @@
 package arriterator
 
 import (
-  "errors"
-  "reflect"
-  "unsafe"
+	"errors"
+	"reflect"
 )
 
 // Iterator is the interface of `arriterator`
 type Iterator interface {
-  // HasNext gets whether the iterator has next element
-  HasNext() bool
-  
-  // Next gets the next elements of the iterator
-  Next() interface{}
+	// HasNext gets whether the iterator has next element
+	HasNext() bool
+
+	// Next gets the next elements of the iterator
+	Next() interface{}
 }
 
 type iterator struct {
-  arr []interface{}
-  
-  idx, size int // iterating index, `arr`'s size
+	arr reflect.Value
+
+	idx, size int // iterating index, `arr`'s size
 }
 
 // New creates an iterator from `val`.
@@ -29,38 +28,38 @@ type iterator struct {
 // and keeps its own index `idx` for iterating,
 // and keeps the size `size` of `arr`.
 func New(val interface{}) (Iterator, error) {
-  if val == nil {
-    return nil, errors.New("nil pointer")
-  }
-  
-  v := reflect.ValueOf(val)
-  if v.Kind() != reflect.Slice && v.Kind() != reflect.Array {
-    return nil, errors.New("non-array or non-slice pointer")
-  }
-  
-  it := &iterator{
-    arr:  *(*[]interface{})(unsafe.Pointer(&val)),
-    idx:  0,
-    size: v.Len(),
-  }
-  return it, nil
+	if val == nil {
+		return nil, errors.New("nil pointer")
+	}
+
+	v := reflect.ValueOf(val)
+	if v.Kind() != reflect.Slice && v.Kind() != reflect.Array {
+		return nil, errors.New("non-array or non-slice pointer")
+	}
+
+	it := &iterator{
+		arr:  v,
+		idx:  0,
+		size: v.Len(),
+	}
+	return it, nil
 }
 
 // HasNext gets whether the iterator has next element of its `arr`.
 // It gets `true`, if its `idx` is less than its `size`.
 func (i *iterator) HasNext() bool {
-  return i != nil && i.idx < i.size
+	return i != nil && i.idx < i.size
 }
 
 // Next gets the next element of the iterator.
 // It gets `nil`, if it's `nil` or its `idx` is not less than its `size`.
 // It gets the `idx` element of `arr`, and increments `idx`.
 func (i *iterator) Next() interface{} {
-  if i == nil || i.idx >= i.size {
-    return nil
-  }
-  
-  ret := i.arr[i.idx]
-  i.idx++
-  return ret
+	if i == nil || i.idx >= i.size {
+		return nil
+	}
+
+	ret := i.arr.Index(i.idx)
+	i.idx++
+	return ret.Interface()
 }
--- arriterator_test.go
+++ arriterator_test.go
@@ -1,121 +1,164 @@
 package arriterator
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestNew(t *testing.T) {
-  i, e := New(nil)
-  if e == nil {
-    t.Log("failed with nil pointer")
-    t.Fail()
-  }
-  
-  i, e = New([]int{})
-  if e != nil {
-    t.Log("failed with integer array")
-    t.Fail()
-  }
-  
-  i, e = New(([]int{1, 2, 3})[:0])
-  if e != nil {
-    t.Log("failed with integer slice")
-    t.Fail()
-  }
-  
-  i, e = New(&[]int{})
-  if e == nil {
-    t.Log("failed with integer array pointer")
-    t.Fail()
-  }
-  
-  i, e = New(map[int]int{})
-  if e == nil {
-    t.Log("failed with integer-integer map")
-    t.Fail()
-  }
-  
-  _ = i
+	i, e := New(nil)
+	if e == nil {
+		t.Log("failed with nil pointer")
+		t.Fail()
+	}
+
+	i, e = New([]int{})
+	if e != nil {
+		t.Log("failed with integer array")
+		t.Fail()
+	}
+
+	i, e = New(([]int{1, 2, 3})[:0])
+	if e != nil {
+		t.Log("failed with integer slice")
+		t.Fail()
+	}
+
+	i, e = New(&[]int{})
+	if e == nil {
+		t.Log("failed with integer array pointer")
+		t.Fail()
+	}
+
+	i, e = New(map[int]int{})
+	if e == nil {
+		t.Log("failed with integer-integer map")
+		t.Fail()
+	}
+
+	_ = i
 }
 
 func TestHasNext(t *testing.T) {
-  var i Iterator
-  if i != nil && i.HasNext() {
-    t.Log("failed with nil Iterator")
-    t.Fail()
-  }
-  
-  i, _ = New(&[]int{})
-  if i != nil && i.HasNext() {
-    t.Log("failed with integer array pointer")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{})
-  if i.HasNext() {
-    t.Log("failed with empty integer array")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{1})
-  if !i.HasNext() {
-    t.Log("failed with non-empty integer array")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{1, 2, 3})
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if i.HasNext() {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
+	var i Iterator
+	if i != nil && i.HasNext() {
+		t.Log("failed with nil Iterator")
+		t.Fail()
+	}
+
+	i, _ = New(&[]int{})
+	if i != nil && i.HasNext() {
+		t.Log("failed with integer array pointer")
+		t.Fail()
+	}
+
+	i, _ = New([]int{})
+	if i.HasNext() {
+		t.Log("failed with empty integer array")
+		t.Fail()
+	}
+
+	i, _ = New([]int{1})
+	if !i.HasNext() {
+		t.Log("failed with non-empty integer array")
+		t.Fail()
+	}
+
+	i, _ = New([]int{1, 2, 3})
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if i.HasNext() {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
 }
 
 func TestNext(t *testing.T) {
-  var i Iterator
-  if i != nil && i.Next() != nil {
-    t.Log("failed with nil Iterator")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{})
-  if i == nil || i.Next() != nil {
-    t.Log("failed with empty integer array")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{1})
-  if i == nil || i.Next() == nil {
-    t.Log("failed with non-empty integer array")
-    t.Fail()
-  }
-  
-  i, _ = New([]int{1, 2, 3})
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if v := i.Next(); v == nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
-  if v := i.Next(); v != nil {
-    t.Log("failed with iterated non-empty integer array")
-    t.Fail()
-  }
+	var i Iterator
+	if i != nil && i.Next() != nil {
+		t.Log("failed with nil Iterator")
+		t.Fail()
+	}
+
+	i, _ = New([]int{})
+	if i == nil || i.Next() != nil {
+		t.Log("failed with empty integer array")
+		t.Fail()
+	}
+
+	i, _ = New([]int{1})
+	if i == nil || i.Next() == nil {
+		t.Log("failed with non-empty integer array")
+		t.Fail()
+	}
+
+	i, _ = New([]int{1, 2, 3})
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if v := i.Next(); v == nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+	if v := i.Next(); v != nil {
+		t.Log("failed with iterated non-empty integer array")
+		t.Fail()
+	}
+}
+
+func TestIterator(t *testing.T) {
+	ints := []int{111, 222, 333}
+	i, e := New(ints)
+	if e != nil {
+		t.Log("failed with non-empty integer array")
+		t.FailNow()
+	}
+	idx := 0
+	for i.HasNext() {
+		v, ok := i.Next().(int)
+		if !ok {
+			t.Log("failed with iterating non-empty integer array")
+			t.FailNow()
+		}
+
+		if v != ints[idx] {
+			t.Log("failed with iterating non-empty integer array")
+			t.FailNow()
+		}
+		idx++
+	}
+
+	strings := []string{"abc", "bcd", "xxx"}
+	i, e = New(strings)
+	if e != nil {
+		t.Log("failed with non-empty string array")
+		t.FailNow()
+	}
+	idx = 0
+	for i.HasNext() {
+		v, ok := i.Next().(string)
+		if !ok {
+			t.Log("failed with iterating non-empty string array")
+			t.FailNow()
+		}
+		if v != strings[idx] {
+			t.Log("failed with iterating non-empty string array")
+			t.FailNow()
+		}
+		idx++
+	}
 }
